5b5874e3cd51976b3df25489cc5383c696670cc2
Ryno Andryano <rynoandryano@gmail.com>
2023-11-02 12:07:09 +0700

    chore: initiate project

---
+++ src/main/java/com/nutech/PpobSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nutech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpobSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpobSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nutech/PpobSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpobSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
